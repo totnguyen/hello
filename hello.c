1b1dfa801c238202df19275a21d908186c917236
tommy nguyen <tnguyen138@huskers.unl.edu>
2020-01-16 11:20:27 -0600

    Name and major
    Added name and major

---
+++ hello.c
@@ -0,0 +1,14 @@
+/*
+* Name: Tommy Nguyen
+* Date: 2020/1/16
+*
+* This program prints out my name and major
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	printf("Tommy Nguyen!\n");
+	printf("Software Engineer\n");
+	return 0;
+}
